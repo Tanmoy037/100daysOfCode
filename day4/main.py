44d7f706710d92f95b44dd6409b0d75cd509f311
Tanmoy037 <tanmoysantra67@gmail.com>
2023-07-30 20:18:25 +0530

    day4 added

---
+++ day4/main.py
@@ -0,0 +1,172 @@
+# Instructions
+# You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+
+# Important, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
+
+# There are many ways of doing this. But to practice what we learnt in the last lesson, you should generate a random number, either 0 or 1. Then use that number to print out Heads or Tails.
+
+# e.g. 1 means Heads 0 means Tails
+
+# Example Output
+# Heads
+# or
+
+# Tails
+
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+import random
+random_number = random.randint(0,1)
+
+if random_number == 0:
+    print("Tails")
+else:
+    print("Heads")
+
+
+
+
+# Instructions
+# You are going to write a program that will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+
+# Important: You are not allowed to use the choice() function.
+
+# Line 8 splits the string names_string into individual names and puts them inside a List called names. For this to work, you must enter all the names as names followed by comma then space. e.g. name, name, name
+
+# Example Input
+# Angela, Ben, Jenny, Michael, Chloe
+# Note: notice that there is a space between the comma and the next name.
+
+# Example Output
+# Michael is going to buy the meal today!
+
+
+# Import the random module here
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+import random
+
+pic_names = len(names)
+
+random_items = random.randint(0, pic_names - 1)
+
+who_will_pay = names[random_items]
+
+print(who_will_pay + " is going to buy the meal today!")
+
+
+
+# Instructions
+# You are going to write a program that will mark a spot with an X.
+
+# In the starting code, you will find a variable called map.
+
+# This map contains a nested list. When map is printed this is what the nested list looks like:
+
+# [['⬜️', '⬜️', '⬜️'],['⬜️', '⬜️', '⬜️'],['⬜️', '⬜️', '⬜️']]
+
+# This is a bit hard to work with. So on lines 6 and 23, we've used this line of code print(f"{row1}\n{row2}\n{row3}" to format the 3 lists to be printed as a 3 by 3 square, each on a new line. 
+
+# ['⬜️', '⬜️', '⬜️']
+
+# ['⬜️', '⬜️', '⬜️']
+
+# ['⬜️', '⬜️', '⬜️']
+
+# Now it looks a bit more like the coordinates of a real map:
+
+
+
+# Your job is to write a program that allows you to mark a square on the map using a two-digit system. 
+
+# The first digit in the input will specify the column (the position on the horizontal axis).
+
+# The second digit in the input will specify the row number (the position on the vertical axis). 
+
+# So an input of 23 should place an X at the position shown below:
+
+
+
+# First, your program must take the user input and convert it to a usable format.
+
+# Next, you need to use that input to update your nested list with an "x". Remember that your nested list map actually looks like this: [['⬜️', '⬜️', '⬜️'],['⬜️', '⬜️', '⬜️'],['⬜️', '⬜️', '⬜️']].
+
+# Example Input 1
+# column 2, row 3 would be entered as:
+
+# 23
+# Example Output 1
+# ['⬜️', '⬜️', '⬜️']
+# ['⬜️', '⬜️', '⬜️']
+# ['⬜️', 'X', '⬜️']
+# Example Input 2
+# column 3, row 1 would be entered as:
+
+# 31
+# Example Output 2
+# ['⬜️', '⬜️', 'X']
+# ['⬜️', '⬜️', '⬜️']
+# ['⬜️', '⬜️', '⬜️']
+
+
+
+
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+horizonal = int (position[0])
+vertical = int (position[1])
+
+
+
+#Write your code above this row 👆
+map [vertical - 1] [horizonal - 1] = "X"
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
+
+
+
+
+
+# Rock Paper Scissors game
+
+
+
+
+
+import random
+
+user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+
+computer_choice = random.randint(0,2)
+print(f"computer chose {computer_choice}")
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number. You lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif user_choice == computer_choice:
+    print("It's a draw!")
