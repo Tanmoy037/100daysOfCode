0181abcd1bdec0e107c16877a023517d936f7b17
Tanmoy037 <tanmoysantra67@gmail.com>
2023-07-31 23:27:55 +0530

    day4 added

---
+++ day5/main.py
@@ -0,0 +1,151 @@
+# Instructions
+# You are going to write a program that calculates the average student height from a List of heights.
+
+# e.g. student_heights = [180, 124, 165, 173, 189, 169, 146]
+
+# The average height can be calculated by adding all the heights together and dividing by the total number of heights.
+
+# e.g.
+
+# 180 + 124 + 165 + 173 + 189 + 169 + 146 = 1146
+
+# There are a total of 7 heights in student_heights
+
+# 1146 ÷ 7 = 163.71428571428572
+
+# Average height rounded to the nearest whole number = 164
+
+# Important You should not use the sum() or len() functions in your answer. You should try to replicate their functionality using what you have learnt about for loops.
+
+# Example Input
+# 156 178 165 171 187
+# In this case, student_heights would be a list that looks like: [156, 178, 165, 171, 187]
+
+# Example Output
+# 171
+
+
+# using for loop 
+
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+total_student = 0
+count = 0
+for student in student_heights:
+    total_student += student 
+    count += 1
+print (round(total_student/count))
+
+
+# Instructions
+# You are going to write a program that calculates the highest score from a List of scores.
+
+# e.g. student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+# Important you are not allowed to use the max or min functions. The output words must match the example. i.e
+
+# The highest score in the class is: x
+
+# Example Input
+# 78 65 89 86 55 91 64 89
+# In this case, student_scores would be a list that looks like: [78, 65, 89, 86, 55, 91, 64, 89]
+
+# Example Output
+# The highest score in the class is: 91
+
+
+
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print (f"The highest score in the class is: {highest_score}")
+
+
+
+
+# Instructions
+# You are going to write a program that automatically prints the solution to the FizzBuzz game.
+
+# Your program should print each number from 1 to 100 in turn.
+
+# When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+
+# When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
+
+#   And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
+
+# e.g. it might start off like this:
+
+# 1
+# 2
+# Fizz
+# 4
+# Buzz
+# Fizz
+# 7
+# 8
+# Fizz
+# Buzz
+# 11
+# Fizz
+# 13
+# 14
+# FizzBuzz
+
+
+#Write your code below this row 👇
+
+for i in range(1, 16):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizz buzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
+
+
+# Password Generator Project
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+# Randomly select characters from each category
+random_letters = ''.join(random.choice(letters) for _ in range(nr_letters))
+random_symbols = ''.join(random.choice(symbols) for _ in range(nr_symbols))
+random_numbers = ''.join(random.choice(numbers) for _ in range(nr_numbers))
+
+# Combine all the randomly selected characters to create the password
+whole_password = random_letters + random_symbols + random_numbers
+
+# Shuffle the password to ensure randomness
+password_list = list(whole_password)
+random.shuffle(password_list)
+shuffled_password = ''.join(password_list)
+
+print("Generated Password:", shuffled_password)
