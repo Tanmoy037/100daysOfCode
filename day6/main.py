2ea93a3614218a2af772eb29cef849fd2c7ba1b3
Tanmoy037 <tanmoysantra67@gmail.com>
2023-08-02 12:11:15 +0530

    day6 complete

---
+++ day6/main.py
@@ -0,0 +1,43 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
+
+
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif frint_is_clear():
+        move()
+    else:
+        turn_left()
