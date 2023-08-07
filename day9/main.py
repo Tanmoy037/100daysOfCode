b19130314407027b0bd4ababf6fccf564924f153
Tanmoy037 <tanmoysantra67@gmail.com>
2023-08-07 08:30:25 +0530

    day9 complete

---
+++ day9/main.py
@@ -0,0 +1,156 @@
+# Instructions
+# You have access to a database of student_scores in the format of a dictionary. The keys in student_scores are the names of the students and the values are their exam scores.
+
+# Write a program that converts their scores to grades. By the end of your program, you should have a new dictionary called student_grades that should contain student names for keys and their grades for values. The final version of the student_grades dictionary will be checked.
+
+# DO NOT modify lines 1-7 to change the existing student_scores dictionary.
+
+# DO NOT write any print statements.
+
+# This is the scoring criteria:
+
+# Scores 91 - 100: Grade = "Outstanding"
+
+# Scores 81 - 90: Grade = "Exceeds Expectations"
+
+# Scores 71 - 80: Grade = "Acceptable"
+
+# Scores 70 or lower: Grade = "Fail"
+
+# Expected Output
+# '{'Harry': 'Exceeds Expectations', 'Ron': 'Acceptable', 'Hermione': 'Outstanding', 'Draco': 'Acceptable', 'Neville': 'Fail'}'
+# Hint
+# Remember that looping through a Dictionary will only give you the keys and not the values.
+
+# If in doubt as to why your code is not doing what you expected, you can always print out the intermediate values.
+
+# At the end of your program, the print statement will show the final student_scores dictionary, do not change this.
+
+# Test Your Code
+# Check your code is doing what it is supposed to. When you're happy with your code, click submit to check your solution.
+
+# Solution
+# https://repl.it/@appbrewery/day-9-1-solution
+
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+#TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+#TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80 and score < 91:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70 and score < 81:
+        student_grades[student] = "Acceptable"
+    elif score < 70:
+        student_grades[student] = "Fail"
+
+    
+
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
+
+
+
+# Instructions
+# You are going to write a program that adds to a travel_log. You can see a travel_log which is a List that contains 2 Dictionaries.
+
+# Write a function that will work with the following line of code on line 21 to add the entry for Russia to the travel_log.
+
+# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+# You've visited Russia 2 times.
+
+# You've been to Moscow and Saint Petersburg.
+
+# DO NOT modify the travel_log directly. You need to create a function that modifies it.
+
+
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+
+def add_new_country(country_visited,times_visits,cities_visited):
+    new_country = {}
+    new_country["country"]= country_visited
+    new_country["visits"]= times_visits
+    new_country["cities"]= cities_visited
+    travel_log.append(new_country)
+
+
+
+
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from replit import clear
+from art import logo
+print(logo)
+
+bids = {}
+bidding_finished = False
+
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  # bidding_record = {"Angela": 123, "James": 321}
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
+      winner = bidder
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bidding_finished:
+  name = input("What is your name?: ")
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if should_continue == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    clear()
+  
+
