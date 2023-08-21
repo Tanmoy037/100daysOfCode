a945a6360ef8f8f3d44a15497ac173ada52c89b8
Tanmoy037 <tanmoysantra67@gmail.com>
2023-08-21 20:09:06 +0530

    day20 complete

--- day20/main.py
+++ day20/main.py
@@ -1,44 +1,28 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
+import time
 
 screen = Screen()
-screen.setup(width=600,height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-new_snake = []
-x = 0
-y = 0
-for _ in range(0,3):
-    turtle_new = Turtle()
-    turtle_new.color("white")
-    turtle_new.shape("square")
-    turtle_new.penup()
-    turtle_new.goto(x, y)
-    new_snake.append(turtle_new)
-    x -= 20
-    
+snake = Snake()
+# food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
 while game_is_on:
-    for snake in new_snake:
-        snake.forward(20)
+    screen.update()
+    time.sleep(0.1)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    snake.move()
 
 
 screen.exitonclick()
